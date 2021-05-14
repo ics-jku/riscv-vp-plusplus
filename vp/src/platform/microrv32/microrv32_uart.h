cc3210b46adf77e8261548eb3031b6ca09fafa50
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2021-05-14 15:39:49 +0200

    add skeleton microrv32 uart extension

--- vp/src/platform/microrv32/microrv32_uart.h
+++ vp/src/platform/microrv32/microrv32_uart.h
@@ -13,10 +13,15 @@ struct MicroRV32UART : public sc_core::sc_module {
 	sc_core::sc_event run_event;
 	char buf = 0;
 
+	// untested
+	Vector<char, 16> rxFifo;
+	bool empty, almostEmpty;
+
 	SC_HAS_PROCESS(MicroRV32UART);
 
 	MicroRV32UART(sc_core::sc_module_name) {
 		tsock.register_b_transport(this, &MicroRV32UART::transport);
+		SC_THREAD(run);
 	}
 
 	void transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
@@ -36,9 +41,25 @@ struct MicroRV32UART : public sc_core::sc_module {
 		        // ignore
 		    } else if (addr == 4) {
 		        *((uint32_t*)ptr) = 1;
-		    }
+			// untested
+		    } else if (addr == 8) {
+				*((uint32_t*)ptr) = rxFifo->pop();
+			} else if(addr == 12) {
+				*((uint32_t*)ptr) = rxFifo->occupancy();
+			}
 		}
 
 		(void)delay;  // zero delay
 	}
+
+	// untested
+	void run() {
+		while (true) {
+			run_event.notify(sc_core::sc_time(200, sc_core::SC_MS));
+			sc_core::wait(run_event);  // 40 times per second by default
+			char newRXChar = rand() + 48;
+			rxFifo.push(newRXChar);
+		}
+	}
+
 };
