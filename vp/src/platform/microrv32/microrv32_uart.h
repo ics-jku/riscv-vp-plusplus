4c30bbc5a60a1c292a77a452a38b5e493cb4292a
Nick Michalek <michalek@uni-bremen.de>
2021-08-16 14:14:19 +0000

    MicroRV32 UART run_event fix

--- vp/src/platform/microrv32/microrv32_uart.h
+++ vp/src/platform/microrv32/microrv32_uart.h
@@ -43,7 +43,7 @@ struct MicroRV32UART : public sc_core::sc_module {
 		auto cmd = trans.get_command();
 		auto len = trans.get_data_length();
 		auto ptr = trans.get_data_ptr();
-		
+
 		if (cmd == tlm::TLM_WRITE_COMMAND) {
 		    if (addr == 0) {
 		        buf = *ptr;
@@ -51,16 +51,21 @@ struct MicroRV32UART : public sc_core::sc_module {
 		        std::cout << buf;
 		    }
 		} else if (cmd == tlm::TLM_READ_COMMAND) {
+			const size_t size = rxFIFO.size();
 		    if (addr == 0) {
 		        // ignore
 		    } else if (addr == 4) {
 		        *((uint32_t*)ptr) = 1;
-			// untested
 		    } else if (addr == 8) {
 				*((uint32_t*)ptr) = rxFIFO.front();
 				rxFIFO.pop();
 			} else if(addr == 12) {
-				*((uint32_t*)ptr) = rxFIFO.size();
+				*((uint32_t*)ptr) = size;
+			} else if(addr == 16) {
+				// almost empty
+			} else if(addr == 20) {
+				// empty
+				*((uint32_t*)ptr) = size == 0;
 			}
 		}
 
@@ -69,24 +74,30 @@ struct MicroRV32UART : public sc_core::sc_module {
 
 	void event_loop(std::function<void(void)> f){
 		while (true) {
-			run_event.notify(sc_core::sc_time(200, sc_core::SC_MS));
+			run_event.notify(sc_core::sc_time(200, sc_core::SC_NS));
 			sc_core::wait(run_event);  // 40 times per second by default
 			f();
 		}
 	}
 
-	// untested
 	void run() {
 		std::ifstream uart_stream;
 	    uart_stream.open(uart_rx_fd_path_, std::ifstream::in);
 		if (uart_stream.is_open()){
+			std::cout << "[info] Reading from UART file stream under path=" << uart_rx_fd_path_ << std::endl;
 			// read from uart
 			event_loop([&](){
-				char new_rx_char = rand() + 48;
-				rxFIFO.push(new_rx_char);
+				char c;
+				while (uart_stream.get(c)){
+					std::cout << c;
+					rxFIFO.push(c);
+				}
+				std::cout << std::endl;
 			});
+			uart_stream.close();
 		} else {
-			std::cerr << "Failed to open file " << uart_rx_fd_path_ << std::endl;
+			std::cerr << "[error] Failed to open UART file stream under path=" << uart_rx_fd_path_ << std::endl;
+			std::cout << "[info] Manually pushing random chars into RX buffer" << std::endl;
 
 			event_loop([&](){
 				char new_rx_char = rand() + 48;
