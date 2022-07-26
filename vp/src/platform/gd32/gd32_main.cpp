ddaf2246e9eb389786ff5586425da153c80f687a
Florian Stoegmueller <stoegmueller.f@gmail.com>
2022-07-26 20:49:29 +0200

    GD32: Added RCU
    Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

--- vp/src/platform/gd32/gd32_main.cpp
+++ vp/src/platform/gd32/gd32_main.cpp
@@ -9,6 +9,7 @@
 #include "mem.h"
 #include "memory.h"
 #include "platform/common/options.h"
+#include "rcu.h"
 #include "spi.h"
 #include "timer.h"
 
@@ -21,6 +22,9 @@ class GD32Options : public Options {
 
 	addr_t timer_start_addr = 0xD1000000;
 	addr_t timer_end_addr = 0xD100D000;
+
+	addr_t rcu_start_addr = 0x40021000;
+	addr_t rcu_end_addr = 0x400213FF;
 
 	addr_t spi_start_addr = 0x40013000;
 	addr_t spi_end_addr = 0x400133FF;
@@ -59,9 +63,10 @@ int sc_main(int argc, char **argv) {
 	SimpleMemory sram("SRAM", opt.sram_size);
 	SimpleMemory flash("Flash", opt.flash_size);
 	ELFLoader loader(opt.input_program.c_str());
-	SimpleBus<2, 5> ahb("AHB");
+	SimpleBus<2, 6> ahb("AHB");
 	CombinedMemoryInterface iss_mem_if("MemoryInterface", core);
 
+	RCU rcu("RCU");
 	TIMER timer("TIMER");
 	GPIO gpioa("GPIOA");
 	SPI spi0("SPI0");
@@ -87,6 +92,7 @@ int sc_main(int argc, char **argv) {
 	ahb.ports[2] = new PortMapping(opt.gpioa_start_addr, opt.gpioa_end_addr);
 	ahb.ports[3] = new PortMapping(opt.timer_start_addr, opt.timer_end_addr);
 	ahb.ports[4] = new PortMapping(opt.spi_start_addr, opt.spi_end_addr);
+	ahb.ports[5] = new PortMapping(opt.rcu_start_addr, opt.rcu_end_addr);
 
 	loader.load_executable_image(flash, flash.size, opt.flash_start_addr, false);
 	loader.load_executable_image(sram, sram.size, opt.sram_start_addr, false);
@@ -101,6 +107,7 @@ int sc_main(int argc, char **argv) {
 	ahb.isocks[2].bind(gpioa.tsock);
 	ahb.isocks[3].bind(timer.tsock);
 	ahb.isocks[4].bind(spi0.tsock);
+	ahb.isocks[5].bind(rcu.tsock);
 
 	std::vector<debug_target_if *> threads;
 	threads.push_back(&core);
---
+++ vp/src/platform/gd32/rcu.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <tlm_utils/simple_target_socket.h>
+
+#include <systemc>
+
+#include "util/tlm_map.h"
+
+struct RCU : public sc_core::sc_module {
+	tlm_utils::simple_target_socket<RCU> tsock;
+
+	// memory mapped configuration registers
+	uint32_t rcu_ctl = 0x83;
+	uint32_t rcu_cfg0 = 0x00;
+	uint32_t rcu_int = 0x00;
+	uint32_t rcu_apb2rst = 0x00;
+	uint32_t rcu_apb1rst = 0x00;
+	uint32_t rcu_ahben = 0x14;
+	uint32_t rcu_apb2en = 0x00;
+	uint32_t rcu_apb1en = 0x00;
+	uint32_t rcu_bdctl = 0x18;
+	uint32_t rcu_rstsck = 0x0C000000;
+	uint32_t rcu_ahbrst = 0x00;
+	uint32_t rcu_cfg1 = 0x00;
+	uint32_t rcu_dsv = 0x00;
+
+	enum {
+		RCU_CTL_REG_ADDR = 0x00,
+		RCU_CFG0_REG_ADDR = 0x04,
+		RCU_INT_REG_ADDR = 0x08,
+		RCU_APB2RST_REG_ADDR = 0x0C,
+		RCU_APB1RST_REG_ADDR = 0x10,
+		RCU_AHBEN_REG_ADDR = 0x14,
+		RCU_APB2EN_REG_ADDR = 0x18,
+		RCU_APB1EN_REG_ADDR = 0x1C,
+		RCU_BDCTL_REG_ADDR = 0x20,
+		RCU_RSTSCK_REG_ADDR = 0x24,
+		RCU_AHBRST_REG_ADDR = 0x28,
+		RCU_CFG1_REG_ADDR = 0x2C,
+		RCU_DSV_REG_ADDR = 0x34,
+	};
+
+	vp::map::LocalRouter router = {"RCU"};
+
+	RCU(sc_core::sc_module_name) {
+		tsock.register_b_transport(this, &RCU::transport);
+
+		router
+		    .add_register_bank({
+		        {RCU_CTL_REG_ADDR, &rcu_ctl},
+		        {RCU_CFG0_REG_ADDR, &rcu_cfg0},
+		        {RCU_INT_REG_ADDR, &rcu_int},
+		        {RCU_APB2RST_REG_ADDR, &rcu_apb2rst},
+		        {RCU_APB1RST_REG_ADDR, &rcu_apb1rst},
+		        {RCU_AHBEN_REG_ADDR, &rcu_ahben},
+		        {RCU_APB2EN_REG_ADDR, &rcu_apb2en},
+		        {RCU_APB1EN_REG_ADDR, &rcu_apb1en},
+		        {RCU_BDCTL_REG_ADDR, &rcu_bdctl},
+		        {RCU_RSTSCK_REG_ADDR, &rcu_rstsck},
+		        {RCU_AHBRST_REG_ADDR, &rcu_ahbrst},
+		        {RCU_CFG1_REG_ADDR, &rcu_cfg1},
+		        {RCU_DSV_REG_ADDR, &rcu_dsv},
+		    })
+		    .register_handler(this, &RCU::register_access_callback);
+	}
+
+	void register_access_callback(const vp::map::register_access_t &r) {}
+
+	void transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
+		router.transport(trans, delay);
+	}
+};
