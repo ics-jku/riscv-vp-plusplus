2ed5e6b0991afc63746c6104d296df9b7500baa3
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2022-10-19 15:14:30 +0200

    change new-uart sw example for mrv32 vp

--- sw/new-uart/main.c
+++ sw/new-uart/main.c
@@ -15,22 +15,25 @@ int main() {
 	delay(10000);
 	char buf[3];
 	char *txt1 = "uart rx failed?!\n";
-	delay(10000);
-	delay(10000);
-	delay(10000);
-	delay(10000);
+	delay(5000);
+	// delay(7000);
+	// delay(2000);
+	// delay(10000);
 
 	char rxBuf[5];
 	int rxEmpty = 1;
-	int maxCnt = 20;
+	int maxCnt = 30;
 	int cnt = 0;
 	rxEmpty = UART->RXEMPT;
+	int occ;
 	if(!rxEmpty) {
 		// read rx data register from uart peripheral
 		do{
 			rxBuf[0] = UART->RXDATA;
 			putChr(rxBuf[0]);
 			cnt++;
+			occ = UART->RXOCCU;
+			// sendString()
 		}while(UART->RXEMPT || cnt <= maxCnt);
 		putChr('\n');
 		putChr('\n');
