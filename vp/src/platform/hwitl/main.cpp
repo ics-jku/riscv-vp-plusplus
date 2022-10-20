7166cb2800f963ea8e1e9085a97dc6ae55ad0a28
Pascal Pieper <ppieper@informatik.uni-bremen.de>
2022-10-20 15:27:39 +0200

    Barebone hardware in the loop vp

---
+++ vp/src/platform/hwitl/main.cpp
@@ -0,0 +1,202 @@
+#include <cstdlib>
+#include <ctime>
+
+#include "core/common/clint.h"
+#include "elf_loader.h"
+#include "fe310_plic.h"
+#include "debug_memory.h"
+#include "iss.h"
+#include "mem.h"
+#include "memory.h"
+#include "syscall.h"
+#include "uart.h"
+#include "util/options.h"
+#include "platform/common/options.h"
+
+#include "gdb-mc/gdb_server.h"
+#include "gdb-mc/gdb_runner.h"
+
+#include <boost/io/ios_state.hpp>
+#include <boost/program_options.hpp>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+
+using namespace rv32;
+namespace po = boost::program_options;
+
+class BasicOptions : public Options {
+public:
+	typedef unsigned int addr_t;
+
+	std::string virtual_bus_device;
+	std::string test_signature;
+
+	addr_t mem_size = 1024 * 1024 * 32;  // 32 MB ram, to place it before the CLINT and run the base examples (assume
+	                                     // memory start at zero) without modifications
+	addr_t mem_start_addr = 0x00000000;
+	addr_t mem_end_addr = mem_start_addr + mem_size - 1;
+	addr_t clint_start_addr = 0x02000000;
+	addr_t clint_end_addr = 0x0200ffff;
+	addr_t sys_start_addr = 0x02010000;
+	addr_t sys_end_addr = 0x020103ff;
+	addr_t uart_start_addr = 0x20010000;
+	addr_t uart_end_addr = uart_start_addr + 0xfff;
+	addr_t plic_start_addr = 0x40000000;
+	addr_t plic_end_addr = 0x41000000;
+
+	bool use_E_base_isa = false;
+
+	OptionValue<unsigned long> entry_point;
+
+	BasicOptions(void) {
+        	// clang-format off
+		add_options()
+			("memory-start", po::value<unsigned int>(&mem_start_addr),"set memory start address")
+			("memory-size", po::value<unsigned int>(&mem_size), "set memory size")
+			("use-E-base-isa", po::bool_switch(&use_E_base_isa), "use the E instead of the I integer base ISA")
+			("entry-point", po::value<std::string>(&entry_point.option),"set entry point address (ISS program counter)")
+			("virtual-bus-device",  po::value<std::string>(&virtual_bus_device),"tty to virtual bus responder");
+        	// clang-format on
+	}
+
+	void parse(int argc, char **argv) override {
+		Options::parse(argc, argv);
+
+		entry_point.finalize(parse_ulong_option);
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const BasicOptions& o) {
+	os << "virtual-bus-device:\t" << o.virtual_bus_device << std::endl;
+	os << static_cast <const Options&>( o );
+	return os;
+}
+
+int sc_main(int argc, char **argv) {
+	BasicOptions opt;
+	opt.parse(argc, argv);
+
+	std::srand(std::time(nullptr));  // use current time as seed for random generator
+
+	tlm::tlm_global_quantum::instance().set(sc_core::sc_time(opt.tlm_global_quantum, sc_core::SC_NS));
+
+	ISS core(0, opt.use_E_base_isa);
+	SimpleMemory mem("SimpleMemory", opt.mem_size);
+	UART uart("Generic_UART", 6);
+	ELFLoader loader(opt.input_program.c_str());
+	SimpleBus<3, 13> bus("SimpleBus");
+	CombinedMemoryInterface iss_mem_if("MemoryInterface", core);
+	SyscallHandler sys("SyscallHandler");
+	FE310_PLIC<1, 64, 96, 32> plic("PLIC");
+	CLINT<1> clint("CLINT");
+	DebugMemoryInterface dbg_if("DebugMemoryInterface");
+
+	MemoryDMI dmi = MemoryDMI::create_start_size_mapping(mem.data, opt.mem_start_addr, mem.size);
+	InstrMemoryProxy instr_mem(dmi, core);
+
+	std::shared_ptr<BusLock> bus_lock = std::make_shared<BusLock>();
+	iss_mem_if.bus_lock = bus_lock;
+
+	instr_memory_if *instr_mem_if = &iss_mem_if;
+	data_memory_if *data_mem_if = &iss_mem_if;
+	if (opt.use_instr_dmi)
+		instr_mem_if = &instr_mem;
+	if (opt.use_data_dmi) {
+		iss_mem_if.dmi_ranges.emplace_back(dmi);
+	}
+
+	uint64_t entry_point = loader.get_entrypoint();
+	if (opt.entry_point.available)
+		entry_point = opt.entry_point.value;
+	try {
+		loader.load_executable_image(mem, mem.size, opt.mem_start_addr);
+	} catch(ELFLoader::load_executable_exception& e) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << "Memory map: " << std::endl;
+		std::cerr << opt << std::endl;
+		return -1;
+	}
+	/*
+	 * The rv32 calling convention defaults to 32 bit, but as this Config is
+	 * mainly used together with the syscall handler, this helps for certain floats.
+	 * https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-elf.adoc
+	 */
+	core.init(instr_mem_if, data_mem_if, &clint, entry_point, rv64_align_address(opt.mem_end_addr));
+	sys.init(mem.data, opt.mem_start_addr, loader.get_heap_addr());
+	sys.register_core(&core);
+
+	if (opt.intercept_syscalls)
+		core.sys = &sys;
+
+	// address mapping
+	{
+		unsigned it = 0;
+		bus.ports[it++] = new PortMapping(opt.mem_start_addr, opt.mem_end_addr);
+		bus.ports[it++] = new PortMapping(opt.clint_start_addr, opt.clint_end_addr);
+		bus.ports[it++] = new PortMapping(opt.plic_start_addr, opt.plic_end_addr);
+		bus.ports[it++] = new PortMapping(opt.uart_start_addr, opt.uart_end_addr);
+		bus.ports[it++] = new PortMapping(opt.sys_start_addr, opt.sys_end_addr);
+	}
+
+	// connect TLM sockets
+	iss_mem_if.isock.bind(bus.tsocks[0]);
+	dbg_if.isock.bind(bus.tsocks[2]);
+
+	{
+		unsigned it = 0;
+		bus.isocks[it++].bind(mem.tsock);
+		bus.isocks[it++].bind(clint.tsock);
+		bus.isocks[it++].bind(plic.tsock);
+		bus.isocks[it++].bind(uart.tsock);
+		bus.isocks[it++].bind(sys.tsock);
+	}
+
+	// connect interrupt signals/communication
+	plic.target_harts[0] = &core;
+	clint.target_harts[0] = &core;
+
+	std::vector<debug_target_if *> threads;
+	threads.push_back(&core);
+
+	core.trace = opt.trace_mode;  // switch for printing instructions
+	if (opt.use_debug_runner) {
+		auto server = new GDBServer("GDBServer", threads, &dbg_if, opt.debug_port);
+		new GDBServerRunner("GDBRunner", server, &core);
+	} else {
+		new DirectCoreRunner(core);
+	}
+
+	sc_core::sc_start();
+
+	core.show();
+
+	if (opt.test_signature != "") {
+		auto begin_sig = loader.get_begin_signature_address();
+		auto end_sig = loader.get_end_signature_address();
+
+		{
+			std::cout << std::hex;
+			std::cout << "begin_signature: " << begin_sig << std::endl;
+			std::cout << "end_signature: " << end_sig << std::endl;
+			std::cout << "signature output file: " << opt.test_signature << std::endl;
+			std::cout << std::dec;
+		}
+
+		assert(end_sig >= begin_sig);
+		assert(begin_sig >= opt.mem_start_addr);
+
+		auto begin = begin_sig - opt.mem_start_addr;
+		auto end = end_sig - opt.mem_start_addr;
+
+		std::ofstream sigfile(opt.test_signature, std::ios::out);
+
+		auto n = begin;
+		while (n < end) {
+			sigfile << std::hex << std::setw(2) << std::setfill('0') << (unsigned)mem.data[n];
+			++n;
+		}
+	}
+
+	return 0;
+}
