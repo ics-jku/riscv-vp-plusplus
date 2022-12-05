b2fed4b17fb17fcf7e2216ff4424a0f03b9067b3
Pascal Pieper <ppieper@informatik.uni-bremen.de>
2022-12-05 13:39:39 +0100

    Fixup of wrong prettyname

--- vp/src/core/common/instr.cpp
+++ vp/src/core/common/instr.cpp
@@ -409,7 +409,7 @@ enum Opcode {
 
 std::array<const char*, 32> Opcode::regnamePrettyStr = {
 	"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-	"s0/fp", "s1", "a0", "a)", "a)", "a3", "a4", "a5",
+	"s0/fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
 	"a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
 	"s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
