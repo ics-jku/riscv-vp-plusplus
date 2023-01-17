vherdt/riscv-vp

commit 02563f378ec57a0f7e3236390ba01c089610ba91

Added more verbose output

vp/src/platform/basic/main.cpp
@@ -100,6 +100,14 @@ public:
 	}
 };
 
+std::ostream& operator<<(std::ostream& os, const BasicOptions& o) {
+	os << std::hex;
+	os << "mem_start_addr:\t" << +o.mem_start_addr << std::endl;
+	os << "mem_end_addr:\t"   << +o.mem_end_addr   << std::endl;
+	os << static_cast <const Options&>( o );
+	return os;
+}
+
 int sc_main(int argc, char **argv) {
 	BasicOptions opt;
 	opt.parse(argc, argv);
@@ -144,8 +152,14 @@ int sc_main(int argc, char **argv) {
 	uint64_t entry_point = loader.get_entrypoint();
 	if (opt.entry_point.available)
 		entry_point = opt.entry_point.value;
-
-	loader.load_executable_image(mem, mem.size, opt.mem_start_addr);
+	try {
+		loader.load_executable_image(mem, mem.size, opt.mem_start_addr);
+	} catch(ELFLoader::load_executable_exception& e) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << "Memory map: " << std::endl;
+		std::cerr << opt << std::endl;
+		return -1;
+	}
 	core.init(instr_mem_if, data_mem_if, &clint, entry_point, rv32_align_address(opt.mem_end_addr));
 	sys.init(mem.data, opt.mem_start_addr, loader.get_heap_addr());
 	sys.register_core(&core);

vp/src/platform/common/options.cpp
@@ -24,6 +24,8 @@ Options::Options(void) {
 	pos.add("input-file", 1);
 }
 
+Options::~Options(){};
+
 void Options::parse(int argc, char **argv) {
 	try {
 		auto parser = po::command_line_parser(argc, argv);
@@ -48,4 +50,16 @@ void Options::parse(int argc, char **argv) {
 			<< std::endl;
 		exit(1);
 	}
+}
+
+std::ostream& operator<<(std::ostream& os, const Options& o) {
+	os << std::dec;
+	os << "intercept_syscalls: " << o.intercept_syscalls << std::endl;
+	os << "use_debug_runner: " << o.use_debug_runner << std::endl;
+	os << "debug_port: " << o.debug_port << std::endl;
+	os << "trace_mode: " << o.trace_mode << std::endl;
+	os << "tlm_global_quantum: " << o.tlm_global_quantum << std::endl;
+	os << "use_instr_dmi: " << o.use_instr_dmi << std::endl;
+	os << "use_data_dmi: " << o.use_data_dmi << std::endl;
+	return os;
 }

vp/src/platform/common/options.h
@@ -6,6 +6,7 @@
 class Options : public boost::program_options::options_description {
 public:
 	Options(void);
+	virtual ~Options();
 	virtual void parse(int argc, char **argv);
 
 	std::string input_program;
@@ -18,10 +19,14 @@ public:
 	bool use_instr_dmi = false;
 	bool use_data_dmi = false;
 
+	friend std::ostream& operator<<(std::ostream& os, const Options& o);
+
 private:
 
 	boost::program_options::positional_options_description pos;
 	boost::program_options::variables_map vm;
 };
+
+std::ostream& operator<<(std::ostream& os, const Options& o);
 
 #endif

vp/src/core/common/elf_loader.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <boost/iostreams/device/mapped_file.hpp>
-
+#include <exception>
 #include <cstdint>
 #include <vector>
 
@@ -19,6 +19,12 @@ struct GenericElfLoader {
 	const char *filename;
 	boost::iostreams::mapped_file_source elf;
 	const Elf_Ehdr *hdr;
+
+	struct load_executable_exception : public std::exception {
+		const char * what () const throw () {
+			return "Tried loading invalid elf layout";
+		}
+	};
 
 	GenericElfLoader(const char *filename) : filename(filename), elf(filename) {
 		assert(elf.is_open() && "file not open");
@@ -75,7 +81,7 @@ struct GenericElfLoader {
 				std::cerr << "Offset overlaps into section:" << std::endl;
 				std::cerr << "\t0x" << std::hex << +addr << " < " << +offset << std::endl;
 				print_phdr(std::cerr, *p, 2);
-				continue;
+				throw load_executable_exception();
 			}
 
 			if (addr + p->p_memsz >= offset + size) {
@@ -84,7 +90,7 @@ struct GenericElfLoader {
 				std::cerr << "\t0x" << std::hex << +addr << " + size 0x" << +p->p_memsz;
 				std::cerr << " would overflow offset 0x" << +offset << " + size 0x" << +size << std::endl;
 				print_phdr(std::cerr, *p, 2);
-				continue;
+				throw load_executable_exception();
 			}
 
 			auto idx = addr - offset;

ics-jku/riscv-vp-plusplus

commit 80be84a938498cc0a6c43e67f74f6c5c9a876f00

Remove overloading of `operator<<`
This re-enables an actual helpful `--help` output.

Fixes regression introduced in 02563f378ec57a0f7e3236390ba01c089610ba91

vp/src/platform/basic/main.cpp
@@ -89,6 +89,13 @@ public:
 			("network-device", po::value<std::string>(&network_device)->default_value(""),"name of the tap network adapter, e.g. /dev/tap6")
 			("signature", po::value<std::string>(&test_signature)->default_value(""),"output filename for the test execution signature");
         	// clang-format on
+	};
+
+	void printValues(std::ostream& os) const override {
+		os << std::hex;
+		os << "mem_start_addr:\t" << +mem_start_addr << std::endl;
+		os << "mem_end_addr:\t"   << +mem_end_addr   << std::endl;
+		static_cast <const Options&>( *this ).printValues(os);
 	}
 
 	void parse(int argc, char **argv) override {
@@ -103,13 +110,6 @@ public:
 	}
 };
 
-std::ostream& operator<<(std::ostream& os, const BasicOptions& o) {
-	os << std::hex;
-	os << "mem_start_addr:\t" << +o.mem_start_addr << std::endl;
-	os << "mem_end_addr:\t"   << +o.mem_end_addr   << std::endl;
-	os << static_cast <const Options&>( o );
-	return os;
-}
 
 int sc_main(int argc, char **argv) {
 	BasicOptions opt;
@@ -161,7 +161,7 @@ int sc_main(int argc, char **argv) {
 	} catch(ELFLoader::load_executable_exception& e) {
 		std::cerr << e.what() << std::endl;
 		std::cerr << "Memory map: " << std::endl;
-		std::cerr << opt << std::endl;
+		opt.printValues(std::cerr);
 		return -1;
 	}
 	/*

vp/src/platform/common/options.cpp
@@ -60,15 +60,14 @@ void Options::parse(int argc, char **argv) {
 	}
 }
 
-std::ostream& operator<<(std::ostream& os, const Options& o) {
+void Options::printValues(std::ostream& os) const {
 	os << std::dec;
-	os << "intercept_syscalls: " << o.intercept_syscalls << std::endl;
-	os << "error-on-zero-traphandler: " << o.error_on_zero_traphandler << std::endl;
-	os << "use_debug_runner: " << o.use_debug_runner << std::endl;
-	os << "debug_port: " << o.debug_port << std::endl;
-	os << "trace_mode: " << o.trace_mode << std::endl;
-	os << "tlm_global_quantum: " << o.tlm_global_quantum << std::endl;
-	os << "use_instr_dmi: " << o.use_instr_dmi << std::endl;
-	os << "use_data_dmi: " << o.use_data_dmi << std::endl;
-	return os;
+	os << "intercept_syscalls: " << intercept_syscalls << std::endl;
+	os << "error-on-zero-traphandler: " << error_on_zero_traphandler << std::endl;
+	os << "use_debug_runner: " << use_debug_runner << std::endl;
+	os << "debug_port: " << debug_port << std::endl;
+	os << "trace_mode: " << trace_mode << std::endl;
+	os << "tlm_global_quantum: " << tlm_global_quantum << std::endl;
+	os << "use_instr_dmi: " << use_instr_dmi << std::endl;
+	os << "use_data_dmi: " << use_data_dmi << std::endl;
 }

vp/src/platform/common/options.h
@@ -2,6 +2,7 @@
 #define RISCV_VP_OPTIONS_H
 
 #include <boost/program_options.hpp>
+#include <iostream>
 
 class Options : public boost::program_options::options_description {
 public:
@@ -20,7 +21,7 @@ public:
 	bool use_instr_dmi = false;
 	bool use_data_dmi = false;
 
-	friend std::ostream& operator<<(std::ostream& os, const Options& o);
+	virtual void printValues(std::ostream& os = std::cout) const;
 
 private:
 
@@ -28,6 +29,5 @@ private:
 	boost::program_options::variables_map vm;
 };
 
-std::ostream& operator<<(std::ostream& os, const Options& o);
 
 #endif