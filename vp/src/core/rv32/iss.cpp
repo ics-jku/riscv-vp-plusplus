34dc5586ffa62913d72d47f7e73d7d73936a2ca7
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2023-02-24 16:21:26 +0100

    Prevent unnecessary WFI resumes: resume only, if interrupts are pending
    According to
    "The RISC-V Instruction Set Manual
    Volume II: Privileged Architecture
    Document Version 20211203" Page 48

--- vp/src/core/rv32/iss.cpp
+++ vp/src/core/rv32/iss.cpp
@@ -1091,8 +1091,11 @@ void ISS::exec_step() {
 			if (u_mode() && csrs.misa.has_supervisor_mode_extension())
 				raise_trap(EXC_ILLEGAL_INSTR, instr.data());
 
-			if (!ignore_wfi && !has_local_pending_enabled_interrupts())
-				sc_core::wait(wfi_event);
+			if (!ignore_wfi) {
+				while (!has_local_pending_enabled_interrupts()) {
+					sc_core::wait(wfi_event);
+				}
+			}
 			break;
 
 		case Opcode::SFENCE_VMA:
--- vp/src/core/rv64/iss.cpp
+++ vp/src/core/rv64/iss.cpp
@@ -1259,6 +1259,12 @@ void ISS::exec_step() {
 			if (u_mode() && csrs.misa.has_supervisor_mode_extension())
 				raise_trap(EXC_ILLEGAL_INSTR, instr.data());
 
+			if (!ignore_wfi) {
+				while (!has_local_pending_enabled_interrupts()) {
+					sc_core::wait(wfi_event);
+				}
+			}
+
 			if (!ignore_wfi && !has_local_pending_enabled_interrupts())
 				sc_core::wait(wfi_event);
 			break;
