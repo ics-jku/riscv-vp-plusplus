c1cecbebb32105fd769ae730704e31564bc43be8
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2023-09-22 17:46:27 +0200

    GD32: VP: added oled (from hifive)
    based on
    vp/src/platform/hifive/oled
    18432c765d
    
    Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

---
+++ vp/src/platform/gd32/oled/common.cpp
@@ -0,0 +1,45 @@
+/*
+ * common.cpp
+ *
+ *  Created on: 24 Sep 2019
+ *      Author: dwd
+ */
+
+#include "common.hpp"
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+namespace ss1106 {
+
+IMPL_ENUM(Operator);
+
+State* getSharedState() {
+	int shmid;
+	for (unsigned i = 0; i < 2; i++) {
+		if ((shmid = shmget(shm_key, sizeof(State), IPC_CREAT | 0660)) >= 0)
+			continue;
+
+		std::cerr << "Could not get " << sizeof(State) << " Byte of shared Memory " << int(shm_key)
+		          << " for oled display" << std::endl;
+		perror("shmget");
+		std::cerr << "Trying again...\n" << std::endl;
+
+		if (-1 == (shmctl(shm_key, IPC_RMID, nullptr))) {
+			std::cerr << "Could not destroy SHM" << std::endl;
+			perror("shmctl");
+			return nullptr;
+		}
+	}
+
+	void* addr = shmat(shmid, nullptr, 0);
+	if (addr == nullptr) {
+		perror("shmat");
+		return nullptr;
+	}
+
+	return reinterpret_cast<State*>(addr);
+}
+
+};  // namespace ss1106
---
+++ vp/src/platform/gd32/oled/oled.hpp
@@ -0,0 +1,45 @@
+/*
+ * oled.hpp
+ *
+ *  Created on: 20 Sep 2019
+ *      Author: dwd
+ *
+ * This class models the SH1106 oled Display driver.
+ */
+
+#pragma once
+#include <functional>
+#include <map>
+
+#include "common.hpp"
+
+class SS1106 {
+   public:
+	typedef std::function<bool()> GetDCPin_function;
+
+   private:
+	static const std::map<ss1106::Operator, uint8_t> opcode;
+
+	struct Command {
+		ss1106::Operator op;
+		uint8_t payload;
+	};
+
+	enum class Mode : uint_fast8_t { normal, second_arg } mode = Mode::normal;
+
+	void* sharedSegment = nullptr;
+	ss1106::State* state;
+
+	Command last_cmd = Command{ss1106::Operator::NOP, 0};
+
+	GetDCPin_function getDCPin;
+
+	uint8_t mask(ss1106::Operator op);
+	Command match(uint8_t cmd);
+
+   public:
+	SS1106(GetDCPin_function getDCPin, ss1106::State* state_memory_override = nullptr);
+	~SS1106();
+
+	uint8_t write(uint8_t byte);
+};
