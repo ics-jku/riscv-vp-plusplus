22ba032bfd8a01be8d7e6b79b425f50f599ce5fa
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2025-01-17 13:37:58 +0100

    vp: platform: linux: improve performance by disabling ignore_wfi
    ignore_wfi was set, which lead to wfi handled as a nop i.e.
    the core is polling and probably no other systemc process
    (e.g. other cores) can run.
    
    With this, ignore_wfi is no longer set, which leads to SystemC-waits on
    wfi and therefore SystemC context switch i.e. other cores can run which
    potentially improves performance.
    
    Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

--- vp/src/platform/linux/linux_main.cpp
+++ vp/src/platform/linux/linux_main.cpp
@@ -309,10 +309,6 @@ int sc_main(int argc, char **argv) {
 		// switch for printing instructions
 		cores[i]->iss.trace = opt.trace_mode;
 
-		// ignore WFI instructions (handle them as a NOP, which is ok according to the RISC-V ISA) to avoid running too
-		// fast ahead with simulation time when the CPU is idle
-		cores[i]->iss.ignore_wfi = true;
-
 		// emulate RISC-V core boot loader
 		cores[i]->iss.regs[RegFile::a0] = cores[i]->iss.get_hart_id();
 		cores[i]->iss.regs[RegFile::a1] = opt.dtb_rom_start_addr;
@@ -329,6 +325,9 @@ int sc_main(int argc, char **argv) {
 	// To prevent this hart from being scheduled when stuck in
 	// the OpenSBI `sbi_hart_hang()` function do not ignore WFI on
 	// this hart.
+	//
+	// NOTE: ignore_wfi is not set by default -> this only has effect, if ignore_wfi is set in
+	// the core initialization loop above
 	//
 	// See: https://github.com/riscv/opensbi/commit/d70f8aab45d1e449b3b9be26e050b20ed76e12e9
 	cores[0]->iss.ignore_wfi = false;
