5891bdfcf3da83f034c291e7bb96385c9ec9dc6d
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2025-09-30 17:24:16 +0200

    vp: util: options: fix multi-platform builds
    Reported by Thomas ARNDT, COSEDA Technologies GmbH
    
    Building for example a library with multiple platforms leads to
    ```
    ld.lld: error: duplicate symbol: parse_ulong_option(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>> const&)
    >>> defined at libics_riscv_vp_support_libraries.a(riscv_tiny32_platform.o)
    >>> defined at libics_riscv_vp_support_libraries.a(riscv_basic32_platform.o)
    ```
    
    Fix: moved parse_ulong_option to other compile unit (options.cpp).
    
    Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

--- vp/src/util/options.h
+++ vp/src/util/options.h
@@ -19,18 +19,4 @@ struct OptionValue {
 	}
 };
 
-unsigned long parse_ulong_option(const std::string &s) {
-	bool is_hex = false;
-	if (s.size() >= 2) {
-		if ((s[0] == '0') && ((s[1] == 'x') || (s[1] == 'X')))
-			is_hex = true;
-	}
-
-	try {
-		if (is_hex)
-			return stoul(s, 0, 16);
-		return stoul(s);
-	} catch (std::exception &e) {
-		throw std::runtime_error(std::string("unable to parse option '") + s + "' into a number");
-	}
-}
+unsigned long parse_ulong_option(const std::string &s);
