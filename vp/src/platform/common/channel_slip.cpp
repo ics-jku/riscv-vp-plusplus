6bceae30fb7006475835e0d310c4e6f4e9ee40e4
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2026-04-15 12:28:43 +0200

    vp: platform: channel_slip: major rework - fixes slip2tun esc handling
    Includes:
     1. Fix for slip2tun escape handling
     2. Major rework and refactoring to improve code comprehensibility
     3. Improved error handling and messages
    
    Regarding the slip2tun fix:
    The original state-less slip2tun conversion was not fully stream-safe.
    For example:
    ping <guest> -s 1000
    or
    ping <guest> -p dbdc  // (ESC, ESC_END)
    lead to dropped packets on the VP side.
    -> Fixed by using a stateful slip2tun conversion.
    
    Signed-off-by: Manfred SCHLAEGL <manfred.schlaegl@gmx.at>

--- vp/src/platform/common/channel_slip.cpp
+++ vp/src/platform/common/channel_slip.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2026 Manfred Schlaegl <manfred.schlaegl@gmx.at>
+ * based on https://github.com/agra-uni-bremen/riscv-vp/commits/master/vp/src/platform/common/slip.{h,cpp}
+ */
+
 #include "channel_slip.h"
 
 #include <fcntl.h>
@@ -8,14 +13,16 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// SLIP (as defined in RFC 1055) doesn't specify an MTU. We therefore
-// subsequently allocate memory for the packet buffer using realloc(3).
-#define SLIP_SNDBUF_STEP 1500
+/*
+ * SLIP (as defined in RFC 1055) doesn't specify an MTU. We therefore
+ * subsequently allocate memory for the packet buffer using realloc(3).
+ */
+#define SLIP_S2T_BUFFER_STEP 100
 
-#define SLIP_END 0300
-#define SLIP_ESC 0333
-#define SLIP_ESC_END 0334
-#define SLIP_ESC_ESC 0335
+#define SLIP_END 0300      // (octal) = 192 (decimal) = 0xC0 (hex)
+#define SLIP_ESC 0333      // (octal) = 219 (decimal) = 0xDB (hex)
+#define SLIP_ESC_END 0334  // (octal) = 220 (decimal) = 0xDC (hex)
+#define SLIP_ESC_ESC 0335  // (octal) = 221 (decimal) = 0xDD (hex)
 
 Channel_SLIP::~Channel_SLIP() {
 	stop();
@@ -29,10 +36,14 @@ int Channel_SLIP::get_mtu(const char *dev) {
 
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (fd == -1) {
+		std::cerr << "CHANNEL_SLIP: ERROR: get_mtu: Failed to create socket (exception): " << strerror(errno)
+		          << std::endl;
 		throw std::system_error(errno, std::generic_category());
 	}
 
 	if (ioctl(fd, SIOCGIFMTU, (void *)&ifr) == -1) {
+		std::cerr << "CHANNEL_SLIP: ERROR: get_mtu: Failed to read mtu (SIOCGIFMTU) (exception): " << strerror(errno)
+		          << std::endl;
 		close(fd);
 		throw std::system_error(errno, std::generic_category());
 	}
@@ -41,10 +52,19 @@ int Channel_SLIP::get_mtu(const char *dev) {
 	return ifr.ifr_mtu;
 }
 
+/************************************************
+ * Channel_FD_IF
+ ************************************************/
+
+/* Channel_FD_IF open_fd: open tun and init */
 int Channel_SLIP::open_fd() {
+	/*
+	 * setup tun
+	 */
 	tunfd = open("/dev/net/tun", O_RDWR);
 	if (tunfd == -1) {
-		goto err0;
+		std::cerr << "CHANNEL_SLIP: ERROR: Failed to open /dev/net/tun: " << strerror(errno) << std::endl;
+		return -1;
 	}
 
 	struct ifreq ifr;
@@ -52,119 +72,173 @@ int Channel_SLIP::open_fd() {
 	ifr.ifr_flags = IFF_TUN | IFF_NO_PI; /* read/write raw IP packets */
 	strncpy(ifr.ifr_name, netdev.c_str(), IFNAMSIZ);
 	if (ioctl(tunfd, TUNSETIFF, (void *)&ifr) == -1) {
-		goto err1;
+		std::cerr << "CHANNEL_SLIP: ERROR: Failed to open " << netdev << ": " << strerror(errno) << std::endl;
+		goto err;
 	}
 
-	sndsiz = 0;
-	if (!(sndbuf = (uint8_t *)malloc(SLIP_SNDBUF_STEP * sizeof(uint8_t)))) {
-		goto err1;
-	}
-	rcvsiz = get_mtu(ifr.ifr_name);
-	if (!(rcvbuf = (uint8_t *)malloc(rcvsiz * sizeof(uint8_t)))) {
-		goto err2;
+	/*
+	 * initialized slip2tun (s2t)
+	 * buffer is allocated on demand
+	 */
+	s2t_buffer_size = 0;
+	s2t_packet_size = 0;
+	s2t_buffer = nullptr;
+	s2t_state_esc = false;
+
+	/* initialize tun2slip (t2s) */
+	t2s_buffer_size = get_mtu(ifr.ifr_name);
+	t2s_buffer = (uint8_t *)malloc(t2s_buffer_size * sizeof(uint8_t));
+	if (t2s_buffer == nullptr) {
+		std::cerr << "CHANNEL_SLIP: ERROR: Allocating tun2slip buffer: " << strerror(errno) << std::endl;
+		goto err;
 	}
 
+	/* ok */
 	return tunfd;
 
-err2:
-	free(sndbuf);
-	sndbuf = NULL;
-err1:
+err:
 	close(tunfd);
-err0:
-	std::system_error(errno, std::generic_category());
 	tunfd = -1;
-
 	return -1;
 }
 
+/* Channel_FD_IF close_fd: reset and close tun */
 void Channel_SLIP::close_fd(int fd) {
-	if (sndbuf) {
-		free(sndbuf);
-		sndbuf = NULL;
+	if (s2t_buffer) {
+		free(s2t_buffer);
+		s2t_buffer = nullptr;
 	}
-	if (rcvbuf) {
-		free(rcvbuf);
-		rcvbuf = NULL;
+	if (t2s_buffer) {
+		free(t2s_buffer);
+		t2s_buffer = nullptr;
 	}
-
 	if (tunfd > 0) {
 		close(tunfd);
+		tunfd = -1;
 	}
 }
 
-void Channel_SLIP::send_packet(void) {
-	if (tunfd < 0) {
-		/* ignore */
+/* Channel_FD_IF write_data: from uart (slip) to outside (tun) */
+void Channel_SLIP::write_data(uint8_t byte) {
+	s2t(byte);
+}
+
+/* Channel_FD_IF handle_input: read from outside (tun) push to uart (slip) */
+void Channel_SLIP::handle_input(int fd) {
+	t2s(fd);
+}
+
+/************************************************
+ * tun2slip
+ ************************************************/
+
+void Channel_SLIP::t2s(int fd) {
+	/* read from tun */
+	ssize_t ret = read(fd, t2s_buffer, t2s_buffer_size);
+	if (ret <= -1) {
+		std::cerr << "CHANNEL_SLIP: TUN2SLIP: WARNING: Reading from tun: " << strerror(errno) << " -> try again"
+		          << std::endl;
 		return;
 	}
 
-	ssize_t ret = write(tunfd, sndbuf, sndsiz);
-	if (ret == -1) {
-		/*
-		 * This happens sometimes -> do not kill VP if this happens
-		 * TODO: rework the whole slip/tun communication: fix it and improve efficiency
-		 */
-		// throw std::system_error(errno, std::generic_category());
-		perror("CHANNEL_SLIP: Error writing to tun:");
-	} else if ((size_t)ret != sndsiz) {
-		throw std::runtime_error("short write");
-	}
-
-	if (sndsiz > SLIP_SNDBUF_STEP && !(sndbuf = (uint8_t *)realloc(sndbuf, SLIP_SNDBUF_STEP))) {
-		throw std::system_error(errno, std::generic_category());
-	}
-	sndsiz = 0;
-}
-
-void Channel_SLIP::handle_input(int fd) {
-	ssize_t ret = read(fd, rcvbuf, rcvsiz);
-	if (ret <= -1) {
-		throw std::system_error(errno, std::generic_category());
-	}
-
+	/* convert packet to slip and push to uart */
 	for (size_t i = 0; i < static_cast<size_t>(ret); i++) {
-		switch (rcvbuf[i]) {
+		switch (t2s_buffer[i]) {
 			case SLIP_END:
+				/* END -> ESC + ESC_END */
 				rxpush(SLIP_ESC);
 				rxpush(SLIP_ESC_END);
 				break;
 			case SLIP_ESC:
+				/* ESC -> ESC -> ESC_ESC */
 				rxpush(SLIP_ESC);
 				rxpush(SLIP_ESC_ESC);
 				break;
 			default:
-				rxpush(rcvbuf[i]);
+				/* push byte */
+				rxpush(t2s_buffer[i]);
 				break;
 		}
 	}
+
+	/* packet complete */
 	rxpush(SLIP_END);
 }
 
-void Channel_SLIP::write_data(uint8_t data) {
-	if (data == SLIP_END) {
-		if (sndsiz > 0) {
-			send_packet();
-		}
+/************************************************
+ * slip2tun
+ ************************************************/
+
+void Channel_SLIP::s2t_send_packet(void) {
+	if (tunfd < 0) {
+		/* ignore */
+		s2t_packet_size = 0;
 		return;
 	}
 
-	if (sndsiz > 0 && sndbuf[sndsiz - 1] == SLIP_ESC) {
-		switch (data) {
-			case SLIP_ESC_END:
-				sndbuf[sndsiz - 1] = SLIP_END;
-				return;
-			case SLIP_ESC_ESC:
-				sndbuf[sndsiz - 1] = SLIP_ESC;
-				return;
-		}
+	if (s2t_packet_size == 0) {
+		/* nothing to do */
+		return;
 	}
 
-	if (sndsiz && sndsiz % SLIP_SNDBUF_STEP == 0) {
-		size_t newsiz = (sndsiz + SLIP_SNDBUF_STEP) * sizeof(uint8_t);
-		if (!(sndbuf = (uint8_t *)realloc(sndbuf, newsiz))) {
+	ssize_t ret = write(tunfd, s2t_buffer, s2t_packet_size);
+	if (ret < 0) {
+		std::cerr << "CHANNEL_SLIP: SLIP2TUN: WARNING: Writing to tun: " << strerror(errno) << " -> ignored"
+		          << std::endl;
+	} else if ((size_t)ret != s2t_packet_size) {
+		std::cerr << "CHANNEL_SLIP: SLIP2TUN: WARNING: Short write to tun -> ignored." << std::endl;
+	}
+}
+
+void Channel_SLIP::s2t(uint8_t byte) {
+	if (byte == SLIP_END) {
+		/* packet complete -> send */
+		s2t_send_packet();
+		/* reset state for next packet */
+		s2t_packet_size = 0;
+		s2t_state_esc = false;
+		return;
+	}
+
+	if (s2t_state_esc) {
+		/* last byte was escape */
+		if (byte == SLIP_ESC_END) {
+			/* ESC + ESC_END -> END */
+			byte = SLIP_END;
+		} else if (byte == SLIP_ESC_ESC) {
+			/* ESC + ESC_ESC -> ESC */
+			byte = SLIP_ESC;
+		} else {
+			std::cerr << "CHANNEL_SLIP: SLIP2TUN: WARNING: Received invalid packet -> dropped" << std::endl;
+			/* reset state for next packet & drop byte */
+			s2t_packet_size = 0;
+			s2t_state_esc = false;
+			return;
+		}
+		/* esc handled */
+		s2t_state_esc = false;
+
+	} else if (byte == SLIP_ESC) {
+		/* remember escape for handling of next byte & drop byte */
+		s2t_state_esc = true;
+		return;
+	}
+
+	/*
+	 * adaptive s2t buffer
+	 * if no more space -> grow by step size
+	 * (never shrink)
+	 */
+	if (s2t_packet_size >= s2t_buffer_size) {
+		s2t_buffer_size += SLIP_S2T_BUFFER_STEP;
+		s2t_buffer = (uint8_t *)realloc(s2t_buffer, s2t_buffer_size);
+		if (s2t_buffer == nullptr) {
+			std::cerr << "CHANNEL_SLIP: SLIP2TUN: ERROR: (Re-)allocating slip2tun buffer (exception): "
+			          << strerror(errno) << std::endl;
 			throw std::system_error(errno, std::generic_category());
 		}
 	}
-	sndbuf[sndsiz++] = data;
+
+	/* add byte to buffer */
+	s2t_buffer[s2t_packet_size++] = byte;
 }
--- vp/src/platform/common/channel_slip.h
+++ vp/src/platform/common/channel_slip.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2026 Manfred Schlaegl <manfred.schlaegl@gmx.at>
+ * based on https://github.com/agra-uni-bremen/riscv-vp/commits/master/vp/src/platform/common/slip.{h,cpp}
+ */
+
 #ifndef RISCV_VP_CHANNEL_SLIP_H
 #define RISCV_VP_CHANNEL_SLIP_H
 
@@ -11,19 +16,28 @@ class Channel_SLIP final : public Channel_FD_IF {
 	virtual ~Channel_SLIP();
 
    private:
-	const std::string netdev;
 	int get_mtu(const char *);
-	void send_packet(void);
-
-	int open_fd() override;
-	void close_fd(int fd) override;
-	void write_data(uint8_t) override;
-	void handle_input(int fd) override;
-
+	const std::string netdev;
 	int tunfd = -1;
 
-	uint8_t *sndbuf = NULL, *rcvbuf = NULL;
-	size_t sndsiz, rcvsiz;
+	/* Channel_FD_IF */
+	int open_fd() override;
+	void close_fd(int fd) override;
+	void write_data(uint8_t byte) override;
+	void handle_input(int fd) override;
+
+	/* slip2tun */
+	uint8_t *s2t_buffer = nullptr;
+	size_t s2t_buffer_size = 0;
+	size_t s2t_packet_size = 0;
+	bool s2t_state_esc = false;
+	void s2t_send_packet(void);
+	void s2t(uint8_t byte);
+
+	/* tun2slip */
+	uint8_t *t2s_buffer = nullptr;
+	size_t t2s_buffer_size = 0;
+	void t2s(int fd);
 };
 
 #endif  // RISCV_VP_CHANNEL_SLIP_H
